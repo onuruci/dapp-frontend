0de212aa957f40adc8849dd9ad8695b5f187bb4a
onuruci <onur.yuce@metu.edu.tr>
2023-01-26 15:00:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+
+import Header from "./components/Header";
+import logoImg from "./common/vendor_logo.png";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+import Intruduction from "./components/Intoduction";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function App() {
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header logoImg={logoImg} />
+      <div
+        style={{ maxWidth: "750px", marginLeft: "auto", marginRight: "auto" }}
+      >
+        <Box sx={{ width: "100%", marginLeft: "auto" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              variant="fullWidth"
+              aria-label="basic tabs example"
+              indicatorColor="secondary"
+              sx={{
+                marginRight: "auto",
+                alignContent: "center",
+              }}
+              centered
+            >
+              <Tab label="Introduction" {...a11yProps(0)} />
+              <Tab label="Vendor" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <Intruduction />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+        </Box>
+      </div>
     </div>
   );
 }
---
+++ src/components/Intoduction/Introduction.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css"
+
+const Introduction = () => {
+  return(
+    <div className="introstyle">
+      <div>A Basic Token Vendor</div>
+      <div>Here we have a GRF Token and each token is worth 0.1 Avax on Fuji testnet. Go check it out and buy some tokens with Fuji-Avax</div>
+      <div>Smart Contract Address: </div>
+      <div>Contact Me: uci.onur@gmail.com</div>
+      <div>Onur</div>
+    </div>
+  );
+};
+
+export default Introduction;
